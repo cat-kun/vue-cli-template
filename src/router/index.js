7e87c1bc87002ed0d6aabc2409254842c9f6cfbe
xiaozhen <626269256@qq.com>
2018-09-17 16:04:38 +0800

    修复在IE9+ import 语法无法渲染路由组件的bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import test from '@/components/test'
 
 Vue.use(Router)
 
@@ -8,11 +10,13 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
+      // component: HelloWorld
       component: () => import('@/components/HelloWorld')
     },
     {
       path: '/test',
       name: 'Test',
+      // component: test
       component: () => import('@/components/test')
     }
   ]
